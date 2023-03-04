aef09cc4bd850f72875b02ae08ad7ed58856dbf1
cesarlopezt <cesarlopez2808@gmail.com>
2023-03-04 16:06:57 -0600

    Initial Commit

---
+++ TemperatureConverter/TemperatureConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  TemperatureConverterApp.swift
+//  TemperatureConverter
+//
+//  Created by Cesar Lopez on 3/4/23.
+//
+
+import SwiftUI
+
+@main
+struct TemperatureConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
