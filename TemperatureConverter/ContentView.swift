3bc1ab1aecd611942ef6e42ee4fa7ad0f74d5d03
cesarlopezt <cesarlopez2808@gmail.com>
2023-03-04 17:06:34 -0600

    Add views and calculations

--- TemperatureConverter/ContentView.swift
+++ TemperatureConverter/ContentView.swift
@@ -8,14 +8,73 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+    @State private var temperature = 20.0
+    @State private var fromMetric = "Celsius"
+    @State private var toMetric = "Fahrenheit"
+    @FocusState private var tempIsFocused
+    
+    private var metrics = ["Celsius", "Fahrenheit", "Kelvin"]
+    private var resultTemp: Double {
+        if (fromMetric == toMetric) { return temperature }
+        if (fromMetric == "Celsius" && toMetric == "Fahrenheit") {
+            return (temperature * 9/5) + 32
         }
-        .padding()
+        if (fromMetric == "Fahrenheit" && toMetric == "Celsius") {
+            return (temperature - 32) * 5/9
+        }
+        if (fromMetric == "Celsius" && toMetric == "Kelvin") {
+            return temperature + 273.15
+        }
+        if (fromMetric == "Kelvin" && toMetric == "Celsius") {
+            return temperature - 273.15
+        }
+        if (fromMetric == "Fahrenheit" && toMetric == "Kelvin") {
+            return (temperature - 32) * 5/9 + 273.15
+        }
+        if (fromMetric == "Kelvin" && toMetric == "Fahrenheit") {
+            return (temperature - 273.15) * 9/5 + 32
+        }
+        return 0
+    }
+    
+    var body: some View {
+        NavigationView {
+            Form {
+                Section {
+                    Picker("Temperature from", selection: $fromMetric) {
+                        ForEach(metrics, id: \.self) {
+                            Text($0)
+                        }
+                    }.pickerStyle(.segmented)
+                    
+                    
+                    TextField("Temperature", value: $temperature, format: .number).keyboardType(.decimalPad).focused($tempIsFocused)
+                } header: {
+                    Text("Temperature from:")
+                }
+                
+                Section {
+                    Picker("Temperature to", selection: $toMetric) {
+                        ForEach(metrics, id: \.self) {
+                            Text($0)
+                        }
+                    }.pickerStyle(.segmented)
+                    Text(resultTemp, format: .number)
+                    
+                } header: {
+                    Text("Temperature to:")
+                }
+            }
+            .navigationBarTitle("Temp. Converter")
+            .toolbar {
+                ToolbarItemGroup(placement: .keyboard) {
+                    Spacer()
+                    Button("OK") {
+                        tempIsFocused = false
+                    }
+                }
+            }
+        }
     }
 }
 
